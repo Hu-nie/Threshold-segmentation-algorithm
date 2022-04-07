eaa6d902bf3a8dab766981d8648be2f384a577b0
Hu-nie <dhshin8227@kyonggi.ac.kr>
2022-04-07 09:30:43 +0900

    <ver1.0>

--- main.py
+++ main.py
@@ -32,69 +32,65 @@ class Cutoff(QThread):
         self.parent.label.setText('load dicom....')
 
         Union_voxel = extract_voxel_data(path)
-        
         flatten_arr= Union_voxel.flatten()
+        voxel_mean, voxel_std = meanStd(Union_voxel)
+        
         self.parent.label.setText('Calculating Normalization......')
 
-        norm_arr, min_v, max_v,_, _ = voxelNorm(Union_voxel)
-        Voxel_mean, Voxel_std = meanStd(Union_voxel)
+        norm_arr, voxel_min, voxle_max = voxelNorm(Union_voxel)
+        
 
         self.parent.label.setText('Calculating Otsu......')
-        normal = Otsu(norm_arr)
+        cutoff, fore_arr = Otsu(norm_arr)
         
         self.parent.label.setText('Calculating Intersection......')
-        normal, n_pdf, g_pdf, inter = getIntersection(int(Weight), normal)
+        fore_arr = deNorm(fore_arr, voxel_min, voxle_max)
+        fore_mean, fore_sd = meanStd(fore_arr)
 
         self.parent.label.setText('Calculating cutoff......')
         
-        p_cut = np.percentile(Union_voxel, percentile)
-        p_norm = (p_cut - Voxel_mean) / Voxel_std
+        newton = newtonRaphson(float(Weight), fore_sd)
         
-        si_cut = deNorm((inter.xy)[0][0],min_v,max_v)
-        si_norm = (si_cut - Voxel_mean) / Voxel_std
-        print(p_cut,inter.xy[0][0],min_v,max_v)
+        
+        p_cut = np.percentile(Union_voxel, percentile)
+        p_norm = (p_cut - voxel_mean) / voxel_std
+        
+        fore_cut = newton*fore_sd+fore_mean
+        fore_norm = (fore_cut - voxel_mean) / voxel_std
+        
         
         duration_time = round((time.time() - start), 5)
 
         self.parent.label.setText('Done')
 
         self.parent.label_3.setText(str(duration_time))
-        self.parent.SI_cut.setText(str(round(si_cut, 5)))
-        self.parent.SI_norm.setText(str(round(si_norm, 5)))
+        self.parent.SI_cut.setText(str(round(fore_cut, 5)))
+        self.parent.SI_norm.setText(str(round(fore_norm, 5)))
         self.parent.P_cut.setText(str(round(p_cut, 5)))
         self.parent.P_norm.setText(str(round(p_norm, 5)))
         plt.clf()
     
-        ax = self.parent.fig.add_subplot(131)
+        ax = self.parent.fig.add_subplot(121)
 
         ax.hist(flatten_arr, bins=500, label='normal', color='midnightblue')
         ax.axvline(round(p_cut, 2), color='red', label='x of percentile 99.5  ={:.3f}'.format(
             round(p_cut, 2)), linestyle='dashed', linewidth=1)
-        ax.axvline(round(si_cut, 2), color='blue', label='x of new_method  ={:.3f}'.format(
-            round(si_cut, 2)), linestyle='dashed', linewidth=1)
+        ax.axvline(round(fore_cut, 2), color='blue', label='x of new_method  ={:.3f}'.format(
+            round(fore_cut, 2)), linestyle='dashed', linewidth=1)
         ax.set_xlabel('Signal intensity')
         ax.set_ylabel('n')
         ax.legend(loc='upper left')
 
-        bx = self.parent.fig.add_subplot(132)
+        bx = self.parent.fig.add_subplot(122)
         bx.hist(flatten_arr, bins=300, label='log',
                 log=True, color='midnightblue')
         bx.axvline(round(p_cut, 2), color='red', label='x of percentile 99.5 ={:.3f}'.format(
             round(p_cut, 2)), linestyle='dashed', linewidth=1)
-        bx.axvline(round(si_cut, 2), color='blue', label='x of new_method  ={:.3f}'.format(
-            round(si_cut, 2)), linestyle='dashed', linewidth=1)
+        bx.axvline(round(fore_cut, 2), color='blue', label='x of new_method  ={:.3f}'.format(
+            round(fore_cut, 2)), linestyle='dashed', linewidth=1)
         bx.set_xlabel('Signal intensity')
         bx.set_ylabel('log(n)')
         bx.legend(loc='upper left')
-
-        cx = self.parent.fig.add_subplot(133)
-        cx.plot(normal, g_pdf, color='black')
-        cx.plot(normal, int(Weight)*n_pdf, color='blue')
-        cx.plot(*inter.xy, 'ro',
-                label='point at x ={:.3f}'.format((inter.xy)[0][0]))
-        cx.set_xlabel('Data points')
-        cx.set_ylabel('Probability Density')
-        cx.legend(loc='upper right')
 
         self.parent.canvas.draw()
 
--- util.py
+++ util.py
@@ -2,10 +2,11 @@ import numpy as np
 import os
 import glob
 import pydicom
-from shapely.geometry import LineString
 from lib.dicom_numpy import *
 import cv2
 import time
+
+
 
 ## 이미지 해상도 확인 후 데이터 3D 배열로 결합
 def extract_voxel_data(path):
@@ -21,78 +22,51 @@ def extract_voxel_data(path):
 
 def Otsu(normArr):
     normal = []
-    for arr in normArr:
-        _ , t_otsu = cv2.threshold(arr, -1, 255,  cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU )
-        tempDiff = cv2.subtract(arr, t_otsu)
-        normal.append(tempDiff)
-        
-    # return normal
-    return np.array(normal)
+    norm_arr= normArr
+    print(normArr.shape)
+    re_arr = normArr.reshape(norm_arr.shape[1]*norm_arr.shape[2],norm_arr.shape[0])
+    cutoff , _ = cv2.threshold(re_arr, -1, 255,  cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU )
     
 
-def meanStd(arr):
-    return np.mean(arr), np.std(arr)
+    for arr in norm_arr:
+        _ , t_otsu = cv2.threshold(arr,cutoff, 255, cv2.THRESH_BINARY_INV)
+        tempDiff = cv2.subtract(arr, t_otsu)
+        normal.append(tempDiff)
+    normal = np.array(normal)
+
+    return cutoff, normal[normal !=0]
 
 def voxelNorm(whole_arr):
     min_v = np.min(whole_arr)
     max_v = np.max(whole_arr)
-    std_v = np.std(whole_arr)
-    mean_v = np.mean(whole_arr)
-    
-    for n, arr in enumerate(whole_arr):
-        whole_arr[n] = ((arr - min_v) * (1 / (max_v - min_v) * 255))
-        whole_arr = whole_arr.astype(np.uint8)
 
-    return whole_arr, min_v, max_v, std_v,mean_v
+    for n, arr in enumerate(whole_arr):
+        whole_arr[n] = 255 * (arr - min_v) * (1 / (max_v - min_v))
+        whole_arr = whole_arr.astype(np.uint16)
+
+    return whole_arr, min_v, max_v
 
 
 def deNorm(norm, min_v, max_v):
-    value = (norm / 255) * (max_v - min_v) + min_v
+    return (norm / 255) * (max_v - min_v) + min_v
 
-    return value
-
-
-def normal_dist(x, mean, sd):
-    return (1 / np.sqrt(2 * (np.pi * sd))) * np.exp(-0.5 * ((x - mean) / sd) ** 2)
-
-
-
-def gumbel_dist(x, mean, sd):
-    return 1 / sd * np.exp(-((x - mean) / sd) - np.exp(-((x - mean) / sd)))
-
-
-def getIntersection(weight, normal):
-
-    arr_nonzero = normal[normal !=0]
-    mean = np.mean(arr_nonzero)
-    std = np.std(arr_nonzero)
-
-    arr2list = np.unique((arr_nonzero.flatten()).tolist())
-    
-    n_pdf = normal_dist(arr2list, mean, std)
-    g_pdf = gumbel_dist(arr2list, mean, std)
-    # print(len(n_pdf))
-    line_1 = LineString(np.column_stack((arr2list, g_pdf)))
-    line_2 = LineString(np.column_stack((arr2list, weight * n_pdf)))
-    inter = line_1.intersection(line_2)
- 
-    return arr2list, n_pdf, g_pdf, inter
 
 def newtonRaphson(weight,sd):
     x = 3
+
     while True:
-        x_numeric = x - ((x**2*0.5)-x-np.exp(-x)+np.log(1/sd)-np.log(weight/np.sqrt(2*np.pi*sd)))/(x-1+ np.exp(-x))
-        if x-x_numeric <= 1e-6: break
-        x = x_numeric
-    return (x_numeric)
-    # # print('근사해: '+ str(x_numeric))
+        d =  ftn(x,sd,weight)/ftn_drv(x)
 
-def intersection(normal,weight):
-    arr_nonzero = normal[normal !=0]
-    mean = np.mean(arr_nonzero)
-    sd = np.std(arr_nonzero)
+        if abs(d) <= 1e-7: break
+        x = x-d    
 
-    for i in range(0,256):
-        if weight*normal_dist(i,mean,sd) <= gumbel_dist(i,mean,sd): break 
-    
-    return i
+    return x
+
+def ftn(x,sd,weight):
+    return (x**2)/2-x-np.exp(-x)+np.log(1/sd)-np.log(weight/np.sqrt(2*np.pi*sd))
+
+def ftn_drv(x):
+    return x-1+np.exp(-x)
+
+def meanStd(arr):
+    return np.mean(arr), np.std(arr)
